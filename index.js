fdd65bacd7e99ff4955e9ce6bc5f72b5f6d82a29
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-07-08 23:13:15 +0600

    MongoDB Url :bus:

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// mongoDB database
+// MongoDB database
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ahe248t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
